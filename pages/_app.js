54d409ba9b6d26389298a4055a88b9ec89e58a39
Chris Kaltwasser <chris@chrisk.com>
2022-11-18 11:13:23 -0500

    set ssr to false

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,11 @@ import { AmplifyProvider } from '@aws-amplify/ui-react'
 import config from '../src/aws-exports'
 import '@aws-amplify/ui-react/styles.css'
 
-Amplify.configure({ ...config, ssr: true })
+Amplify.configure({ ...config, ssr: false })
 function MyApp({ Component, pageProps }) {
 	return (
         <>
-            <h1>ArcaSea Video</h1>
+            <h1>ArcaSea Team Videos</h1>
             <AmplifyProvider>
                 <Component {...pageProps} />
             </AmplifyProvider>
